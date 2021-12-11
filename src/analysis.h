e944c98b6e52c19cc0459bbb810061043cac35dd
Jakob Stark <jakob@fa-stark.de>
2021-12-11 01:23:28 +0100

    added analysis module (not yet implemented)

---
+++ src/analysis.h
@@ -0,0 +1,54 @@
+#ifndef ANALYSIS_H
+#define ANALYSIS_H
+
+#include "simulate.h"
+#include "mc.h"
+
+/**
+ * @defgroup analysis analysis
+ *
+ * Detailed description
+ * 
+ * @todo add more documentation
+ *
+ * @{
+ */
+
+/**
+ * @brief Structure to hold all the input information, that can be passed to
+ *        the program
+ */
+typedef struct {
+    char * player_names[4];             /**< pointer to null terminated strings describing the players */
+    uint8_t computer_player_id;         /**< which of the players is the one we are going to simulate */
+    uint8_t starting_player_id;         /**< which of the players began the game */
+    CardId played_cards[48];            /**< all the played cards in order */
+    uint8_t played_cards_len;           /**< length of the played cards array */
+    CardId computer_player_hand[12];    /**< current hand cards of the computer player */
+    uint8_t computer_player_hand_len;   /**< length of the current hand cards array */
+}
+
+
+
+} InputInfo;
+
+/** @brief Analyzes the input
+ *
+ *  This function is used to convert the input into the internal data
+ *  structures of type GameInfo and CardInfo. If the input describes a valid
+ *  card distribution, the function fills the game_dest and card_info
+ *  structures with appropriate data to be passed to the core functions. If the
+ *  input describes an invalid game history (e.g. invalid card distribution or
+ *  errorneous play of one of the players) the function returns a nonzero
+ *  value.
+ *
+ *  @param input Pointer to input, that is to be analyzed.
+ *  @param[out] game_info This structure is filled with GameInfo data.
+ *  @param[out] card_info This structure is filled with CardInfo data.
+ *  @return 0 on success, a nonzero value if the input is invalid.
+ */
+int analyze( const InputInfo* input, GameInfo* game_info, CardInfo* card_info );
+
+/**@}*/
+
+#endif
