8717dafbb9af9aa3861b1c03e0f5d14cee31d898
Jakob Stark <jakob@fa-stark.de>
2024-05-07 15:50:32 +0200

    added poseidon, a first nontrivial agent

---
+++ src/common.hpp
@@ -0,0 +1,267 @@
+#ifndef COMMON_HPP_INCLUDED
+#define COMMON_HPP_INCLUDED
+
+#include <protocol/interface.hpp>
+
+extern "C" {
+#include "analysis.h"
+#include "mc.h"
+#include "random.h"
+#include "simulate.h"
+}
+
+namespace doko::convert {
+
+using namespace protocol;
+
+inline PlayerId player(player_t const& player) {
+    switch (player) {
+    case player1:
+        return 0;
+    case player2:
+        return 1;
+    case player3:
+        return 2;
+    case player4:
+        return 3;
+    }
+    throw rpc_exception{app_code_t::conversion_failed, "invalid player_t"};
+}
+
+inline player_t player_r(PlayerId player) {
+    switch (player) {
+    case 0:
+        return player1;
+    case 1:
+        return player2;
+    case 2:
+        return player3;
+    case 3:
+        return player4;
+    }
+    throw rpc_exception{app_code_t::conversion_failed, "invalid PlayerId"};
+}
+
+inline CardId card(card_t const& card) {
+    switch (card.suit) {
+    case diamond:
+        switch (card.value) {
+        case nine:
+            return DIAMOND_NINE_L;
+        case jack:
+            return DIAMOND_JACK_L;
+        case queen:
+            return DIAMOND_QUEEN_L;
+        case king:
+            return DIAMOND_KING_L;
+        case ten:
+            return DIAMOND_TEN_L;
+        case ace:
+            return DIAMOND_ACE_L;
+        }
+    case heart:
+        switch (card.value) {
+        case nine:
+            return HEART_NINE_L;
+        case jack:
+            return HEART_JACK_L;
+        case queen:
+            return HEART_QUEEN_L;
+        case king:
+            return HEART_KING_L;
+        case ten:
+            return HEART_TEN_L;
+        case ace:
+            return HEART_ACE_L;
+        }
+    case spade:
+        switch (card.value) {
+        case nine:
+            return SPADE_NINE_L;
+        case jack:
+            return SPADE_JACK_L;
+        case queen:
+            return SPADE_QUEEN_L;
+        case king:
+            return SPADE_KING_L;
+        case ten:
+            return SPADE_TEN_L;
+        case ace:
+            return SPADE_ACE_L;
+        }
+    case club:
+        switch (card.value) {
+        case nine:
+            return CLUB_NINE_L;
+        case jack:
+            return CLUB_JACK_L;
+        case queen:
+            return CLUB_QUEEN_L;
+        case king:
+            return CLUB_KING_L;
+        case ten:
+            return CLUB_TEN_L;
+        case ace:
+            return CLUB_ACE_L;
+        }
+    }
+    throw rpc_exception{app_code_t::conversion_failed, "invalid card_t"};
+}
+
+inline card_t card_r(CardId card) {
+    switch (card) {
+    case CLUB_NINE_L:
+    case CLUB_NINE_H:
+        return {club, nine};
+    case CLUB_JACK_L:
+    case CLUB_JACK_H:
+        return {club, jack};
+    case CLUB_QUEEN_L:
+    case CLUB_QUEEN_H:
+        return {club, queen};
+    case CLUB_KING_L:
+    case CLUB_KING_H:
+        return {club, king};
+    case CLUB_TEN_L:
+    case CLUB_TEN_H:
+        return {club, ten};
+    case CLUB_ACE_L:
+    case CLUB_ACE_H:
+        return {club, ace};
+
+    case SPADE_NINE_L:
+    case SPADE_NINE_H:
+        return {spade, nine};
+    case SPADE_JACK_L:
+    case SPADE_JACK_H:
+        return {spade, jack};
+    case SPADE_QUEEN_L:
+    case SPADE_QUEEN_H:
+        return {spade, queen};
+    case SPADE_KING_L:
+    case SPADE_KING_H:
+        return {spade, king};
+    case SPADE_TEN_L:
+    case SPADE_TEN_H:
+        return {spade, ten};
+    case SPADE_ACE_L:
+    case SPADE_ACE_H:
+        return {spade, ace};
+
+    case HEART_NINE_L:
+    case HEART_NINE_H:
+        return {heart, nine};
+    case HEART_JACK_L:
+    case HEART_JACK_H:
+        return {heart, jack};
+    case HEART_QUEEN_L:
+    case HEART_QUEEN_H:
+        return {heart, queen};
+    case HEART_KING_L:
+    case HEART_KING_H:
+        return {heart, king};
+    case HEART_TEN_L:
+    case HEART_TEN_H:
+        return {heart, ten};
+    case HEART_ACE_L:
+    case HEART_ACE_H:
+        return {heart, ace};
+
+    case DIAMOND_NINE_L:
+    case DIAMOND_NINE_H:
+        return {diamond, nine};
+    case DIAMOND_JACK_L:
+    case DIAMOND_JACK_H:
+        return {diamond, jack};
+    case DIAMOND_QUEEN_L:
+    case DIAMOND_QUEEN_H:
+        return {diamond, queen};
+    case DIAMOND_KING_L:
+    case DIAMOND_KING_H:
+        return {diamond, king};
+    case DIAMOND_TEN_L:
+    case DIAMOND_TEN_H:
+        return {diamond, ten};
+    case DIAMOND_ACE_L:
+    case DIAMOND_ACE_H:
+        return {diamond, ace};
+    }
+    throw rpc_exception{app_code_t::conversion_failed, "invalid CardId"};
+}
+
+}; // namespace doko::convert
+
+namespace doko::random {
+
+struct RandomGenerator {
+    using result_type = uint32_t;
+
+    static constexpr result_type max() { return UINT32_MAX; }
+    static constexpr result_type min() { return 0; }
+
+    result_type operator()() { return random_bits(&state); }
+
+    result_type state{1};
+};
+
+} // namespace doko::random
+
+namespace doko::protocol {
+
+struct BasicAgent : doko::protocol::agent_if {
+    PlayerId computer_player_id{};
+    PlayerId starting_player_id{};
+    std::array<CardId, 12> computer_player_cards{};
+
+    GameInfo game_info{};
+    CardInfo card_info{};
+
+    random::RandomGenerator rnd{};
+
+    void initialize(player_t computer_player, player_t starting_player,
+                    std::array<card_t, 12> cards) final {
+        computer_player_id = convert::player(computer_player);
+        starting_player_id = convert::player(starting_player);
+
+        for (auto i = 0; i < cards.size(); ++i) {
+            computer_player_cards[i] = convert::card(cards[i]);
+        }
+    };
+
+    game_t get_announcement() final { return {}; };
+
+    void start(game_t game, player_t starting_player) final {
+        if (game != normal) {
+            throw rpc_exception{app_code_t::not_implemented,
+                                "ares support only normal games"};
+        }
+
+        starting_player_id = convert::player(starting_player);
+        if (analysis_start(&game_info, &card_info, computer_player_id,
+                           starting_player_id,
+                           computer_player_cards.data()) != 0) {
+            throw rpc_exception{app_code_t::gameplay_error, "analysis_start"};
+        }
+    };
+
+    player_t do_move(player_t player, move_t move) final {
+        if (analysis_move(&game_info, &card_info, computer_player_id,
+                          convert::player(player),
+                          convert::card(move.card)) != 0) {
+            throw rpc_exception{app_code_t::gameplay_error, "analysis_move"};
+        }
+        return convert::player_r((game_info.next + computer_player_id) % 4);
+    };
+
+    int finish() final {
+        int points{};
+        if (analysis_finish(&game_info, &points) != 0) {
+            throw rpc_exception{app_code_t::gameplay_error, "analysis_finish"};
+        }
+        return points;
+    }
+};
+
+}; // namespace doko::protocol
+
+#endif
--- src/poseidon.cpp
+++ src/poseidon.cpp
@@ -1,9 +1,63 @@
-#include <iostream>
-#include <format>
+#include <protocol/interface.hpp>
 
-#include "game.hpp"
+#include "common.hpp"
+
+extern "C" {
+#include "mc.h"
+#include "simulate.h"
+}
+
+#include <iostream>
+
+using namespace doko::protocol;
+using namespace doko;
+
+struct Poseidon : doko::protocol::BasicAgent {
+    std::string get_name() final { return "poseidon"; }
+
+    move_t get_move() final {
+        std::array<CardId, 12> legal_cards{};
+        auto length = GetLegalCards(&game_info, legal_cards.data());
+        if (length <= 0 or length > 12) {
+            throw rpc_exception{app_code_t::gameplay_error, "get_legal_cards"};
+        }
+
+        if ( sort_and_check(&card_info) != 0 ) {
+            throw rpc_exception{app_code_t::gameplay_error, "sort_and_check"};
+        }
+
+        std::array<unsigned, 12> results{};
+        unsigned mc_calls{};
+
+        uint32_t random_state{1};
+        for ( int n = 0; n < 10000; n++ ) {
+            GameInfo game_info_copy = game_info;
+
+            mc_calls++;
+            mc_sample(&game_info_copy, &card_info, &random_state);
+
+            for ( uint8_t c = 0; c < length; c++ ) {
+                results[c] += Simulate(&game_info_copy, legal_cards[c], &random_state);
+            }
+        }
+
+        int64_t max_score = -1;
+        CardId max_card = 0;
+
+        for ( uint8_t c = 0; c < length; c++ ) {
+            if ( results[c] > max_score ) {
+                max_score = results[c];
+                max_card = legal_cards[c];
+            }
+        }
+
+        std::cerr << double(max_score)/double(mc_calls) << ' ' << card_names_long[max_card / 2] << '\n';
+
+        return {convert::card_r(max_card), {}};
+    };
+};
 
 int main() {
-    return 0;
-
+    Poseidon agent;
+    doko::protocol::run(agent);
 }
